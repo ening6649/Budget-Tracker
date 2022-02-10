23add550ca575b82f9b55c00761b234a9d76680f
ening <ening6649@yahoo.com>
2022-02-10 16:01:11 -0500

    added manifest.json

---
+++ public/js/idb.js
@@ -0,0 +1,89 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+  // save a reference to the database 
+  const db = event.target.result;
+  db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+// upon a successful 
+request.onsuccess = function(event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run checkDatabase() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadPizza();
+  }
+};
+
+request.onerror = function(event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+// this function will be used in the add-pizza.js form submission function if fetch().catch() is executed
+// ...catch() only executes on network failure
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions 
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  // access the object store for `new_pizza`
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  // add record to your store with add method.
+  pizzaObjectStore.add(record);
+}
+
+function uploadPizza() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  // access your pending object store
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  // get all records from store and set to a variable
+  // .getAll() is asynchronous and it has to be attached to an event handler
+  // ..in order to retrieve the data 
+  const getAll = pizzaObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    // once getAll is completed, getAll will have a .result property that is an array
+    // .. of all the data we retrieved from the new_pizza object store.
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_pizza'], 'readwrite');
+          // access the new_pizza object store
+          const pizzaObjectStore = transaction.objectStore('new_pizza');
+          // clear all items in your store
+          pizzaObjectStore.clear();
+        })
+        .catch(err => {
+          // set reference to redirect back here
+          console.log(err);
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadPizza);
---
+++ public/service-worker.js
@@ -0,0 +1,72 @@
+const APP_PREFIX = 'FoodEvent-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./events.html",
+  "./tickets.html",
+  "./schedule.html",
+  "./assets/css/style.css",
+  "./assets/css/bootstrap.css",
+  "./assets/css/tickets.css",
+  "./dist/app.bundle.js",
+  "./dist/events.bundle.js",
+  "./dist/tickets.bundle.js",
+  "./dist/schedule.bundle.js"
+];
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
+
+// Cache resources
+// install service worker 
+self.addEventListener('install', function (e) {
+  // ensures that the service worker doesnt move on from installin g phase until it s 
+  // ..executed all of its code 
+  e.waitUntil(
+    // find specific cache by name, then add every file in the files to cache array to cache
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    // .keys returns an array of all cache names to Keylist
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  );
+});

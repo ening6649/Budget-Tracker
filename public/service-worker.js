ba073ad0fb8bdae80263954c87af62a995d2d8df
ening <ening6649@yahoo.com>
2022-02-12 13:02:44 -0500

    bug fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,14 +32,14 @@ self.addEventListener('fetch', function (e) {
         .then(response => {
           // If the response was good, clone it and store it in the cache.
           if (response.status === 200) {
-            cache.put(evt.request.url, response.clone());
+            caches.put(evt.request.url, response.clone());
           }
 
           return response;
         })
         .catch(err => {
           // Network request failed, try to get it from the cache.
-          return cache.match(evt.request);
+          return caches.match(evt.request);
         });
       }
 
